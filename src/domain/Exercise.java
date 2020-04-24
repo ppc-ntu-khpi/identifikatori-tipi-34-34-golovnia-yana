9095e4741f6a9201c09ad672aaab023504c0f188
Golovnia Yana <55803221+34-golovnia-yana@users.noreply.github.com>
2020-04-24 09:06:24 +0300

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,10 +1,29 @@
 package domain;
+/**
+ * The class that calculates
+ * @author Asus
+ *
+ */
 
 public class Exercise {
+	/**
+	 * Calculates values in different numerals
+	 * @param number
+	 * @return String
+	 */
     public static String[] Calculate(int number){
         return new String[] {
+        		/**
+        		 * Binary number system
+        		 */
         		Integer.toBinaryString(number),
+        		/**
+        		 * Octal number system
+        		 */
         		Integer.toOctalString(number),
+        		/**
+        		 * Hexadecimal number system
+        		 */
         		Integer.toHexString(number).toUpperCase()
         };
     }
